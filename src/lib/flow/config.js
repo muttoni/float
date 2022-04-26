1012adc19063ff7592731aea4f4d4a069e77f097
Jacob Tucker <tsnakejake@aol.com>
2022-04-26 13:38:48 -0500

    testnet

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -1,15 +1,15 @@
 import { config } from "@samatech/onflow-fcl-esm";
 
 config({
-  "accessNode.api": "https://mainnet.onflow.org", // import.meta.env.VITE_ACCESS_NODE_API,
-  "discovery.wallet": "https://flow-wallet.blocto.app/authn", // import.meta.env.VITE_DISCOVERY_WALLET,
-  "0xFLOAT": "0x2d4c3caffbeab845",
-  "0xCORE": "0x1d7e57aa55817448",
-  "0xFLOWTOKEN": "0x1654653399040a61",
-  "0xFUNGIBLETOKEN": "0xf233dcee88fe0abe",
-  "0xFN": "0x233eb012d34b0070",
-  "0xFIND": "0x097bafa4e0b48eef"
+  "accessNode.api": "https://testnet.onflow.org", // import.meta.env.VITE_ACCESS_NODE_API,
+  "discovery.wallet": "https://flow-wallet-testnet.blocto.app/authn", // import.meta.env.VITE_DISCOVERY_WALLET,
+  "0xFLOAT": "0x0afe396ebc8eee65",
+  "0xCORE": "0x631e88ae7f1d7c20",
+  "0xFLOWTOKEN": "0x7e60df042a9c0868",
+  "0xFUNGIBLETOKEN": "0x9a0766d93b6608b7",
+  "0xFN": "0xb05b2abb42335e88",
+  "0xFIND": "0xa16ab1d0abde3625"
 })
 
-export const verifiersIdentifier = 'A.2d4c3caffbeab845';
-export const flowTokenIdentifier = 'A.1654653399040a61';
+export const verifiersIdentifier = 'A.0afe396ebc8eee65';
+export const flowTokenIdentifier = 'A.7e60df042a9c0868';
